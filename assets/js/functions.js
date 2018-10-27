22a00d723ef3c4058131c5b3b5747ba984381054
dunja-Bday <33137083+dunja-Bday@users.noreply.github.com>
2018-10-27 16:11:44 +0200

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,41 +1,89 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Oct 28, 2017 19:50:00").getTime();
-//19 50
-// Update the count down every 1 second
-var state=false;
-var x = setInterval(function() {
- 
-  // Get todays date and time
-  var now = new Date().getTime();
+var date1=null;
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-  
-  
-  // Time calculations for days, hours, minutes and seconds
+//add data recieving system
+var dateRecieved=false;
+//temporary
 
-  var days = Math.floor((distance  / (1000 * 60 * 60 * 24)));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var name;
+var startYear=2000;
+var year=2000;
+var month=10;
+var day=28;
 
-  // Display the result in the element with id="demo"
-    if(!state)
+var hour=0;
+var minute=0;
+
+var threshold=3600*24;
+var overTrue=false;
+//how often does this repeat
+var intervalYear=1;
+var intervalMonth=0;
+var intervalDay=0;
+var intervalTrue=true;
+//when it should warn you about countdown
+var warningTrue=false;
+var warningDay;
+var warningHour;
+var warningMinute;
+setInterval(frame,1000);
+
+function frame()//runs every 1s
+{
+    if(date1==null)
         {
-  document.getElementById("timer").innerHTML = + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-        }
-    else 
-        {
-            
+            date1=moment(year.toString()+("0"+month.toString()).slice(-2)+("0"+day.toString()).slice(-2)+" "+("0"+hour.toString()).slice(-2)+":"+("0"+minute.toString()).slice(-2), "YYYYMMDD HH:mm");
         }
     
-  // If the count down is finished, write some text 
-  if (distance < 0) {
     
-    document.getElementById("timer").innerHTML = "!~Happy Birthday~!";
-      if(distance<-1000*60*60){
-        countDownDate=new Date("Oct 28, "+((new Date()).getFullYear()+1).toString()+" 19:50:00").getTime();
-      }
-  }
-}, 1000);
+   var days=moment().diff(date1,"days")*(-1);
+var hours=moment().diff(date1,"hours")*(-1);
+var min=moment().diff(date1,"minutes")*(-1);
+var sec=moment().diff(date1,"seconds")*(-1);
+     
+        if(sec<0)//ending
+        {
+            if(Math.abs(sec)>threshold&&intervalTrue)//its over lets wait a nother year
+                {
+                    //interval
+                    TimerNext();
+                    return;
+                }
+            else//party time or interval false and the timer is finished
+                {
+                    if(overTrue)
+                        {
+                            return;
+                        }
+                   TimerOver();
+                }
+        
+        }
+    else
+        {
+            DisplayTime(days,hours,min,sec);
+        }
+
+}
+function DisplayTime(days,hours,min,sec)
+{
+    $("#timer").empty();
+    $("#timer").append(days+"d "+hours%24+"h "+min%60+"m "+sec%60+"s ");
+}
+function TimerOver()
+{
+    var years=moment().year()-startYear;
+    
+     overTrue=true;
+     $("#timer").empty();
+    $("#timer").append("~~Happy "+years+"th Birthday~~");
+}
+function TimerNext()
+{
+    year+=intervalYear;
+    month+=intervalMonth;
+    day+=intervalDay;
+    overTrue=false;
+
+    date1=moment(year.toString()+("0"+month.toString()).slice(-2)+("0"+day.toString()).slice(-2)+" "+("0"+hour.toString()).slice(-2)+":"+("0"+minute.toString()).slice(-2), "YYYYMMDD HH:mm");
+    frame();
+}
